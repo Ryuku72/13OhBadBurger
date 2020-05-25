7cbab175823fe768d5099b32d94e42ee7d8ccd5a
Joshua K Bader <jaybshinsen@hotmail.com>
2020-05-26 07:46:17 +0800

    another attempt to fix by removing prevent default

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -46,9 +46,8 @@ $(document).ready(function () {
   //Create burger
 
   $(".newBurger").on("submit", function (event) {
-    event.preventDefault();
+    //event.preventDefault();
     event.stopPropagation(); //stop from being sent 3 times to server
-
     $(".newBurger").attr('disabled', true);
   
 
